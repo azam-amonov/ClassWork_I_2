839c0335c36c4f15ef2ad630c9c17b7d4aa645b4
Azam Amonov <azamamonov555@gmail.com>
2023-10-18 20:54:53 +0500

    ooppopopokpj3toierngoeriubgiernbvibtg

---
+++ ClassWork_55_00/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ClassWork_55_00;
+
+Console.WriteLine("Hello, World!");
+var finder = new FileFinder();
+var result = finder.FindFile("/Users/azamammon",
+                "some.txt").ToList();
+foreach (var item in result)
+{
+    Console.WriteLine(item);
+}
