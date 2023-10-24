8b119806a227145f505f9a0e5f0b3e89fb526a59
Azam Amonov <azamamonov555@gmail.com>
2023-10-24 18:50:50 +0500

    lesson commits

---
+++ Mapper/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Mapper.Service;
+using Sharprompt;
+
+Console.WriteLine("Hello, World!");
+var service = new UserService();
+Console.WriteLine("Hello, World!");
